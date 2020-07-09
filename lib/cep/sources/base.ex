2e34110f0e9092c7595304a750876c4436545483
felipe.piacsek <felipe.piacsek@gmail.com>
2020-07-09 14:21:03 -0300

    base.ex: Adds to_address function to deserialize Cep.Address

--- lib/cep/sources/base.ex
+++ lib/cep/sources/base.ex
@@ -16,6 +16,23 @@ defmodule Cep.Sources.Base do
     end
   end
 
+  def to_address(result) do
+    %Cep.Address{
+      street: Map.get(result, "logradouro"),
+      neighborhood: Map.get(result, "bairro"),
+      complement: Map.get(result, "complemento"),
+      cep: Map.get(result, "cep"),
+      city: infer(:city, result),
+      state: infer(:state, result)
+    }
+  end
+
+  defp infer(:city, %{"localidade" => city}), do: city
+  defp infer(:city, %{"cidade" => city}), do: city
+  defp infer(:state, %{"estado" => state}), do: state
+  defp infer(:state, %{"uf" => state}), do: state
+  defp infer(_, _), do: nil
+
   def cep_not_found_error do
     {:not_found, "CEP not found."}
   end
