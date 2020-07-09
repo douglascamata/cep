89d258d5234e4637896fe4caed4ee3a0ed43f2d4
felipe.piacsek <felipe.piacsek@gmail.com>
2020-07-09 16:15:52 -0300

    Fixing exconstructor removal.

--- lib/cep/address.ex
+++ lib/cep/address.ex
@@ -1,4 +1,3 @@
 defmodule Cep.Address do
   defstruct [:street, :neighborhood, :city, :state, :complement, :cep]
-  use ExConstructor
 end
--- lib/cep/source.ex
+++ lib/cep/source.ex
@@ -1,4 +1,6 @@
 defmodule Cep.Source do
+  alias Cep.Address
+
   @callback get_address(cep :: String.t()) ::
               {:ok, %Cep.Address{}} | {:not_found, String.t()} | {:error, String.t()}
 end
